4ac786a85771ba21fd5aaa99477c1d1a5fbba71c
Qingzhi Luo <tomrony97@outlook.com>
2019-12-23 03:02:50 -0800

    Initial Commit

---
+++ TipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by Qingzhi Luo on 2019/12/23.
+//  Copyright © 2019 Qingzhi Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
